7c4f9f0b0af31357c2eeb790ba41943dac2b9158
Stephen Odebiyi <odebiyistephen@gmail.com>
2023-03-18 23:49:45 +0100

    Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   root: true,
   env: {
@@ -21,13 +20,6 @@ module.exports = {
       },
       rules: {
         'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
   ],
